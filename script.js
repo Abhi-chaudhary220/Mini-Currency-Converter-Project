9d5b88eef298acd2c0f7d9bf027c457b764b1aca
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-08-20 20:13:11 +0530

    appending currency code in a dropdown select element

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let amt = document.querySelector("#input");
 let countryFlag1 = document.querySelector("#flag1");
 let countryFlag2 = document.querySelector("#flag2");
-let from = document.querySelector("#fromCurrency");
-let to = document.querySelector("#toCurrency");
+let dropDown = document.querySelectorAll(".dropdown select");
 let button = document.querySelector("#btn");
 let message = document.querySelector("#msgText");
 
 
 let url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/eur.json";
+
+for(let select of dropDown){
+    for(let currCode in currencyCode){
+        let options = document.createElement("option");
+        options.innerText = currCode;
+        options.value = currCode;
+        select.appendChild(options);
+    }
+}

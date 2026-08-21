4c63a5c016e3bed22343ee50ce4ba294cc6161f1
Abhi-chaudhary220 <abhichaudhary.dev@gmail.com>
2026-08-21 05:36:24 +0530

    finally rate conversion project is ready for use

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let amt = document.querySelector("#input");
-let countryFlag1 = document.querySelector("#flag1");
-let countryFlag2 = document.querySelector("#flag2");
 let dropDown = document.querySelectorAll(".dropdown select");
 let button = document.querySelector("#btn");
+let from = document.querySelector("#fromCurrency");
+let to = document.querySelector("#toCurrency");
 let message = document.querySelector("#msgText");
 
-
-let url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/eur.json";
-
+let API_KEY = "f3e2d3bc4004708e317c0b97";
+let Base_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest`;
 for(let select of dropDown){
     for(let currCode in currencyCode){
         let options = document.createElement("option");
@@ -16,9 +15,40 @@ for(let select of dropDown){
         if(select.id ==="fromCurrency" && currCode === "USD"){
             options.selected = "selected";
         }
-        if(select.id === "toCurrency" && currCode === "INR"){
+        else if(select.id === "toCurrency" && currCode === "INR"){
             options.selected = "selected";
         }
         select.appendChild(options)
     }
-}
+    select.addEventListener("change",(event)=>{
+        updateFlg(event.target);
+
+    })
+};
+
+const updateFlg = (select) => {
+    let currCode = select.value;
+    let countryCode = currencyCode[currCode];
+    let link = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+    let image = select.parentElement.querySelector("img");
+    image.src = link;
+};
+
+button.addEventListener("click", async(event)=>{
+    event.preventDefault();
+    let amtVal = amt.value;
+    if(amtVal === "" || amtVal < 1){
+        amtVal = 1;
+        amt.value = "1";
+    }
+
+
+let URL = `${Base_URL}/${from.value}`;
+let response = await fetch(URL);
+let data = await response.json();
+let rate = data.conversion_rates[to.value];
+let finalRate =  amtVal * rate;
+message.innerText = `${amtVal} ${from.value} = ${finalRate.toFixed(2)} ${to.value}`;
+
+});
+
